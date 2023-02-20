33af96f135a0013068a7a03787d30dff2cec74bc
Nikolaj Sommer <nikolaj.jensen@gmail.com>
2023-02-20 21:15:42 +0100

    added test coverage

--- tests/SimpleMeasurementTest.php
+++ tests/SimpleMeasurementTest.php
@@ -3,8 +3,42 @@ use PHPUnit\Framework\TestCase;
 
 class SimpleMeasurementTest extends TestCase {
 
-    public function testTwoPlusTwoIsFour() {
-        $this->assertEquals(4, 2 + 2);
+    public function testCanMeasureCode() {
+        $measurement = new \Nsommer89\PhpExecMeasure\SimpleMeasurement();
+        $measurement->fn(function () {
+            $a = 1;
+            $b = 2;
+            $c = $a + $b;
+        });
+        $result = $measurement->getResult();
+        $this->assertIsFloat($result->getDuration());
     }
 
+    public function testCanMeasureFile() {
+        exec('php ' . __DIR__ . '/../bin/measure -f tests/SimpleMeasurementTest.php', $output, $return_var);
+
+        $this->assertEquals(0, $return_var);
+    }
+
+    public function testToJson() {
+        $measurement = new \Nsommer89\PhpExecMeasure\SimpleMeasurement();
+        $measurement->fn(function () {
+            $a = 1;
+            $b = 2;
+            $c = $a + $b;
+        });
+        $result = $measurement->getResult();
+        $this->assertJson($result->toJson());
+    }
+
+    public function testToArray() {
+        $measurement = new \Nsommer89\PhpExecMeasure\SimpleMeasurement();
+        $measurement->fn(function () {
+            $a = 1;
+            $b = 2;
+            $c = $a + $b;
+        });
+        $result = $measurement->getResult();
+        $this->assertIsArray($result->toArray());
+    }
 }
